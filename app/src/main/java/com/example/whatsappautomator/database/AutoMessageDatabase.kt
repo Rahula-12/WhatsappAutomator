4424151a334b3524365d4d2ee6499be7bab40745
rahul <ra9853044@gmail.com>
2024-01-01 00:36:31 +0530

    implemented autoMessageModule

--- app/src/main/java/com/example/whatsappautomator/database/AutoMessageDatabase.kt
+++ app/src/main/java/com/example/whatsappautomator/database/AutoMessageDatabase.kt
@@ -1,4 +1,12 @@
 package com.example.whatsappautomator.database
 
-class AutoMessageDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.whatsappautomator.model.AutoMessage
+
+@Database(entities=[AutoMessage::class], version = 1, exportSchema = false)
+abstract class AutoMessageDatabase(context:Context): RoomDatabase() {
+    abstract fun getAutoMessageDao():AutoMessageDao
+
 }
