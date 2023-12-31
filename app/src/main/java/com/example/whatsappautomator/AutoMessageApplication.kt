7817b9d4393d04b3ace6a16da3d92fe9805f64dd
rahul <ra9853044@gmail.com>
2023-12-31 21:32:25 +0530

    used hilt in project

---
+++ app/src/main/java/com/example/whatsappautomator/AutoMessageApplication.kt
@@ -0,0 +1,9 @@
+package com.example.whatsappautomator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AutoMessageApplication():Application() {
+
+}
