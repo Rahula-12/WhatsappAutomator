4342ad94dea69e173afd302894428b82c792c11f
rahul <ra9853044@gmail.com>
2024-01-01 22:38:29 +0530

    implemented autoMessageRepository

---
+++ app/src/main/java/com/example/whatsappautomator/repository/AutoMessageRepository.kt
@@ -0,0 +1,15 @@
+package com.example.whatsappautomator.repository
+
+import com.example.whatsappautomator.database.AutoMessageDao
+import com.example.whatsappautomator.model.AutoMessage
+import javax.inject.Inject
+
+class AutoMessageRepository @Inject constructor(private val autoMessageDao: AutoMessageDao) {
+
+    suspend fun insertAutoMessage(autoMessage: AutoMessage)=autoMessageDao.insertAutoMessage(autoMessage)
+
+    suspend fun deleteAutoMessage(autoMessage: AutoMessage)=autoMessageDao.deleteAutoMessage(autoMessage)
+
+    fun getAllAutoMessages()=autoMessageDao.getAllMessages()
+
+}
