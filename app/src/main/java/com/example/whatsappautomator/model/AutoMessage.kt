1da31a2ce6f07764477879a2350e7b6f237d7e06
rahul <ra9853044@gmail.com>
2024-01-17 19:31:33 +0530

    added new fields countryCode and workId

--- app/src/main/java/com/example/whatsappautomator/model/AutoMessage.kt
+++ app/src/main/java/com/example/whatsappautomator/model/AutoMessage.kt
@@ -3,14 +3,17 @@ package com.example.whatsappautomator.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.work.Worker
 import java.util.UUID
 
 @Entity
-data class AutoMessage(
+ class AutoMessage(
     @ColumnInfo(name="s_no")
     @PrimaryKey
     val messageNo:String=UUID.randomUUID().toString(),
     val message:String,
     val to:String,
-    val time:String
+    val time:String,
+    val countryCode:String="",
+    val workId:String=""
 )
