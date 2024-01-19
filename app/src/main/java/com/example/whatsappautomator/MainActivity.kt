a6dd3383be3fc60b278b062b237c16d5b6ca373f
rahul <ra9853044@gmail.com>
2024-01-19 22:38:10 +0530

    made changes in dialog box

--- app/src/main/java/com/example/whatsappautomator/MainActivity.kt
+++ app/src/main/java/com/example/whatsappautomator/MainActivity.kt
@@ -16,13 +16,20 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.example.whatsappautomator.model.AutoMessage
+import com.example.whatsappautomator.scheduling.SendMessageWorker
 import com.example.whatsappautomator.services.WhatsAppAccessibilityService
-import com.example.whatsappautomator.ui.theme.AutoMessageApp
 import com.example.whatsappautomator.ui.theme.WhatsappAutomatorTheme
 import com.example.whatsappautomator.viewModel.AutoMessageViewModel
-import dagger.hilt.android.AndroidEntryPoint
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 import com.google.i18n.phonenumbers.Phonenumber
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -38,6 +45,7 @@ class MainActivity : ComponentActivity() {
             startActivity(intent)
         }
         //val viewModel:AutoMessageViewModel=hiltViewModel<>()
+        val workManager=WorkManager.getInstance(applicationContext)
         val phoneNumberUtil=PhoneNumberUtil.getInstance()
         setContent {
             val viewModel= viewModel<AutoMessageViewModel>()
@@ -50,27 +58,64 @@ class MainActivity : ComponentActivity() {
                     AutoMessageApp(
                         autoMessages = viewModel.allMessages.collectAsState().value,
                         addMessage = {
-                            if(it.message.isNullOrEmpty() || it.to.isNullOrEmpty() || it.time.isNullOrEmpty())
-                                Toast.makeText(this,"Please enter correct data",Toast.LENGTH_LONG).show()
+                            if(it.message.isEmpty()) {
+                                Toast.makeText(this,"Please enter message",Toast.LENGTH_LONG).show()
+                            }
+                            else if(it.to.isNullOrEmpty()) {
+                                Toast.makeText(this,"Please enter phone number",Toast.LENGTH_LONG).show()
+                            }
+                            else if(it.countryCode.isNullOrEmpty()) {
+                                Toast.makeText(this,"Please enter county code",Toast.LENGTH_LONG).show()
+                            }
                             else {
                                 val rawNumber = Phonenumber.PhoneNumber()
-                                rawNumber.countryCode=91
+                                rawNumber.countryCode=it.countryCode.toIntOrNull()?:91
                                 rawNumber.nationalNumber = it.to.toLong()
-                                if (phoneNumberUtil.isValidNumber(rawNumber))
+                                if (phoneNumberUtil.isValidNumber(rawNumber)) {
+                                    val workRequest: PeriodicWorkRequest = periodicWorkRequest(it)
+                                    workManager.enqueueUniquePeriodicWork(it.messageNo, ExistingPeriodicWorkPolicy.REPLACE, workRequest)
                                     viewModel.insertMessage(it)
+                                    return@AutoMessageApp true
+                                }
                                 else
-                                    Toast.makeText(this,"Please enter correct data",Toast.LENGTH_LONG).show()
+                                    Toast.makeText(this,"Please enter correct phone number",Toast.LENGTH_LONG).show()
                             }
+                            return@AutoMessageApp false
                         },
                         deleteAutoMessage = {
-
+                            workManager.cancelAllWorkByTag(it.messageNo)
                             viewModel.deleteMessage(it)
-
                         }
                     )
                 }
             }
         }
+    }
+    private fun periodicWorkRequest(it: AutoMessage): PeriodicWorkRequest {
+        val calendar: Calendar = Calendar.getInstance()
+        val nowMillis: Long = calendar.timeInMillis
+
+        calendar.set(Calendar.HOUR_OF_DAY, it.time.subSequence(0, 2).toString().toInt())
+        calendar.set(Calendar.MINUTE, it.time.substring(3).toInt())
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        if (calendar.before(Calendar.getInstance())) {
+            calendar.add(Calendar.DATE, 1)
+        }
+
+        val diff: Long = calendar.timeInMillis - nowMillis
+
+        return PeriodicWorkRequest.Builder(
+            SendMessageWorker::class.java,
+            24,
+            TimeUnit.HOURS,
+            PeriodicWorkRequest.MIN_PERIODIC_FLEX_MILLIS,
+            TimeUnit.MILLISECONDS
+        )
+            .setInitialDelay(diff, TimeUnit.MILLISECONDS)
+            .addTag(it.messageNo)
+            .build()
     }
 }
 
