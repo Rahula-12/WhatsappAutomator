a23e8c9391b0d51e8dd1cf940876abbe01b085ff
rahul <ra9853044@gmail.com>
2024-01-21 21:25:01 +0530

    Application completed partially

--- app/src/main/java/com/example/whatsappautomator/services/WhatsAppAccessibilityService.kt
+++ app/src/main/java/com/example/whatsappautomator/services/WhatsAppAccessibilityService.kt
@@ -1,11 +1,25 @@
 package com.example.whatsappautomator.services
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo.FEEDBACK_AUDIBLE
+import android.accessibilityservice.AccessibilityServiceInfo.FEEDBACK_GENERIC
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 
+
 class WhatsAppAccessibilityService: AccessibilityService() {
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        serviceInfo.apply {
+            eventTypes=AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED
+            packageNames= arrayOf("com.whatsapp")
+            feedbackType= FEEDBACK_GENERIC
+            notificationTimeout=100
+            description
+        }
+    }
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
         if(rootInActiveWindow ==null)   return
         val rootNodeInfo:AccessibilityNodeInfoCompat=AccessibilityNodeInfoCompat.wrap(rootInActiveWindow)
