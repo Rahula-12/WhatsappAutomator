02455b63825277c5365256bae629cae8ab768d90
rahul <ra9853044@gmail.com>
2024-03-30 00:46:54 +0530

    added synchronization effectively

--- app/src/main/java/com/example/whatsappautomator/scheduling/SendMessageWorker.kt
+++ app/src/main/java/com/example/whatsappautomator/scheduling/SendMessageWorker.kt
@@ -4,28 +4,46 @@ import android.app.KeyguardManager
 import android.content.Context
 import android.os.PowerManager
 import android.util.Log
+import androidx.work.CoroutineWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.whatsappautomator.services.WhatsAppAutomateService
 
 
-class SendMessageWorker(val context: Context, workerParameters: WorkerParameters):Worker(context,workerParameters){
+class SendMessageWorker(val context: Context, workerParameters: WorkerParameters):
+    CoroutineWorker(context,workerParameters){
     companion object {
-        const val WAKE_LOCK_TAG = "MyWakeLockTag"
+        var serviceStarted=false
     }
-    override fun doWork(): Result {
+    override suspend fun doWork(): Result {
         val keyguardManager =
             applicationContext.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-            val isDeviceLocked = keyguardManager != null && keyguardManager.isKeyguardLocked
-            if(isDeviceLocked)  return Result.retry()
+            while(keyguardManager.isKeyguardLocked) {
+
+            }
+            while (serviceStarted) {
+
+            }
         return try {
 //            val powerManager = applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
 //            val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"myapp:mywakelocktag")
 //            wakeLock.acquire(60 * 1000L)
             val phoneNumber=inputData.getString("phoneNumber")
             val message=inputData.getString("message")
-            WhatsAppAutomateService.startActionAutomateWhatsApp(context,phoneNumber!!,message!!)
+//            if(!serviceStarted) {
+//                synchronized(this) {
+//                    if (!serviceStarted) {
+                        serviceStarted=true
+                        WhatsAppAutomateService.startActionAutomateWhatsApp(
+                            context,
+                            phoneNumber!!,
+                            message!!
+                        )
+//                    }
+//                }
+//            }
             Log.d("WorkerTest","Test passed")
+           // serviceStarted=false
 //            wakeLock.release()
             Result.success()
         } catch (e:Exception) {
--- app/src/main/java/com/example/whatsappautomator/services/WhatsAppAccessibilityService.kt
+++ app/src/main/java/com/example/whatsappautomator/services/WhatsAppAccessibilityService.kt
@@ -5,6 +5,7 @@ import android.accessibilityservice.AccessibilityServiceInfo.FEEDBACK_GENERIC
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+import com.example.whatsappautomator.scheduling.SendMessageWorker
 
 
 class WhatsAppAccessibilityService: AccessibilityService() {
@@ -20,26 +21,29 @@ class WhatsAppAccessibilityService: AccessibilityService() {
         }
     }
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
-        if(rootInActiveWindow ==null)   return
-        val rootNodeInfo:AccessibilityNodeInfoCompat=AccessibilityNodeInfoCompat.wrap(rootInActiveWindow)
-        val messageNodeList:List<AccessibilityNodeInfoCompat>?= rootNodeInfo.findAccessibilityNodeInfosByViewId("com.whatsapp:id/entry")
-        if(messageNodeList.isNullOrEmpty())  return
-        val messageField=messageNodeList[0]
-        if(messageField.text.isNullOrEmpty() || !messageField.text.endsWith("   ")) return
-        val sendMessageNodeList:List<AccessibilityNodeInfoCompat>?= rootNodeInfo.findAccessibilityNodeInfosByViewId("com.whatsapp:id/send")
-        if(sendMessageNodeList.isNullOrEmpty())  return
-        val sendButton=sendMessageNodeList[0]
-        if(!sendButton.isVisibleToUser) return
-        sendButton.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-        try{
-            Thread.sleep(3000)
-            performGlobalAction(GLOBAL_ACTION_BACK)
-            Thread.sleep(2000)
-        }
-        catch(e:InterruptedException) {
+            if (rootInActiveWindow == null) return
+            val rootNodeInfo: AccessibilityNodeInfoCompat =
+                AccessibilityNodeInfoCompat.wrap(rootInActiveWindow)
+            val messageNodeList: List<AccessibilityNodeInfoCompat>? =
+                rootNodeInfo.findAccessibilityNodeInfosByViewId("com.whatsapp:id/entry")
+            if (messageNodeList.isNullOrEmpty()) return
+            val messageField = messageNodeList[0]
+            if (messageField.text.isNullOrEmpty() || !messageField.text.endsWith("   ")) return
+            val sendMessageNodeList: List<AccessibilityNodeInfoCompat>? =
+                rootNodeInfo.findAccessibilityNodeInfosByViewId("com.whatsapp:id/send")
+            if (sendMessageNodeList.isNullOrEmpty()) return
+            val sendButton = sendMessageNodeList[0]
+            if (!sendButton.isVisibleToUser) return
+            sendButton.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                try {
+                    Thread.sleep(3600)
+                    performGlobalAction(GLOBAL_ACTION_BACK)
+                    Thread.sleep(2000)
+                } catch (e: InterruptedException) {
 
-        }
-        performGlobalAction(GLOBAL_ACTION_BACK)
+                }
+                performGlobalAction(GLOBAL_ACTION_BACK)
+        SendMessageWorker.serviceStarted=false
     }
 
     override fun onInterrupt() {
