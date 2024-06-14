554c3fca9f8d08c2333bf04f57fa419307d0df29
rahul <ra9853044@gmail.com>
2024-06-14 22:38:22 +0530

    created notification channel inside application class

--- app/src/main/java/com/example/whatsappautomator/AutoMessageApplication.kt
+++ app/src/main/java/com/example/whatsappautomator/AutoMessageApplication.kt
@@ -1,6 +1,8 @@
 package com.example.whatsappautomator
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
@@ -16,5 +18,12 @@ class AutoMessageApplication : Application(),Configuration.Provider {
         get() = Configuration.Builder()
             .setWorkerFactory(hiltWorkerFactory)
             .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        val notificationChannel=NotificationChannel("success_channel","Message Delivered",NotificationManager.IMPORTANCE_HIGH)
+        val notificationManager=getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
 
 }
